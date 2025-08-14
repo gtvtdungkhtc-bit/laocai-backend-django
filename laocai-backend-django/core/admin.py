f4882e77e143b156b41e1b0d88aaad7caa2b6443
gtvtdungkhtc-bit <gtvt.dungkhtc@gmail.com>
2025-08-14 15:51:45 +0700

    chore: remove duplicated folder

--- laocai-backend-django/core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Road, Asset, Project, UserProfile
-admin.site.register(Road)
-admin.site.register(Asset)
-admin.site.register(Project)
-admin.site.register(UserProfile)
